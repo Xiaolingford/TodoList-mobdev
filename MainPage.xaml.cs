08d89bb4cd97de109a7ebdc7d0a664c84d32bae7
Xiao Meow <unravedao@gmail.com>
2025-03-03 22:28:00 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Todo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTogglePasswordVisibility(object sender, EventArgs e)
+        {
+            PasswordEntry.IsPassword = !PasswordEntry.IsPassword;
+            TogglePasswordButton.Text = PasswordEntry.IsPassword ? "👁" : "👁‍🗨";
+        }
+
+      
+    }
+}
+
